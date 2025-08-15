76059e2698ab916842b582c0b250339a01f388c5
YuliaGrigorieva <miss000hara@gmail.com>
2025-08-15 21:23:28 +0300

    improve code style

--- src/VoximplantKitChatModule.ts
+++ src/VoximplantKitChatModule.ts
@@ -6,9 +6,6 @@ import { NativeModules, TurboModuleRegistry } from 'react-native';
 import VoximplantKitChat from './NativeVoximplantKitChat';
 
 let turboModule = TurboModuleRegistry ? VoximplantKitChat : null;
-
-let VoximplantKitChatModule = turboModule
-  ? turboModule
-  : NativeModules.VoximplantKitChat;
+let VoximplantKitChatModule = turboModule ?? NativeModules.VoximplantKitChat;
 
 export default VoximplantKitChatModule;
